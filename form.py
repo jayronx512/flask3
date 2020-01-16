1e29feebbaf9e9bc07d5b609d3832452317c2a04
Yeow Jing Jong <yeowjingjong@gmail.com>
2020-01-16 17:27:59 +0800

    initial commit for form flask

---
+++ form.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    first_name = request.args.get('first_name')
+    return render_template('_index.html', first_name=first_name)
+
+@app.route("/contact")
+def contact():
+    return render_template('_contact.html')
+
+if __name__ == '__main__':
+    app.run()
