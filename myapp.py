5a0142e74c9fbf2e1c0304fa0cf55af8c9127d49
Yeow Jing Jong <yeowjingjong@gmail.com>
2020-01-17 10:24:10 +0800

    input hot reloading

---
+++ myapp.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    first_name = request.args.get('first_name')
+    last_name = request.args.get('last_name')
+    return render_template('_index.html', first_name=first_name, last_name = last_name)
+
+@app.route("/contact")
+def contact():
+    return render_template('_contact.html', methods = ["POST"])
+
+if __name__ == '__main__':
+    app.run()
